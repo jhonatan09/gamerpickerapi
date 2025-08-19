03110d71cbabbe206a667471cd70257955910603
jhonatan-acesso <jhonatan.moreira@acessoenergia.com.br>
2025-08-19 12:22:58 -0300

    change port

--- index.js
+++ index.js
@@ -66,7 +66,5 @@ app.get("/specs", async (req, res) => {
   }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("up on " + PORT));
